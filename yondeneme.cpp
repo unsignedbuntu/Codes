9b9caff12c028337114ccc3262551b5e0d7d4314
atalay <103396827+unsignedbuntu@users.noreply.github.com>
2022-10-03 21:00:37 +0300

    Add files via upload

---
+++ yondeneme.cpp
@@ -0,0 +1,56 @@
+ #include <stdio.h>
+
+int main()
+{
+int a,b,x=0,y=0;
+char choice;
+
+printf(" x'i'girin");
+scanf("%d",&x);
+
+printf(" y'yi'girin");
+scanf("%d",&y);
+
+	
+    while(scanf("%c",&choice))
+    {
+for(a=0;a<x;a++)	
+{
+	for(b=0;a+b<y;b++)	
+	{
+	printf(" ");	
+	}
+	
+		for(b=0;a+b<10;b++)
+		{
+		printf("\xdb");
+		b++;
+		}
+		
+	printf("\n");
+}
+        if(choice=='w')
+        {
+            x--;
+            printf("*");
+
+        }
+        else if(choice=='s')
+        {
+            x++;
+        printf("*");
+
+        }
+        else if(choice=='a')
+        {
+            y--;
+            printf("*");	
+		}
+        else if(choice=='d')
+        {
+            y++;
+            printf("*");
+    	}
+     }
+            return 0;
+        }
